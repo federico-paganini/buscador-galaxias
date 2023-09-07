c618cae99c71c61bf77270a6b6d28c311d9c01a5
beaglecoding <matiasmartindossantos@gmail.com>
2023-09-07 20:03:16 -0300

    final con el +

--- js/space.js
+++ js/space.js
@@ -8,17 +8,25 @@ document.addEventListener('DOMContentLoaded', () => {
         const inputBuscar= document.getElementById("inputBuscar").value;
 
         const response= await fetch (`https://images-api.nasa.gov/search?q=${inputBuscar}`)
-        const data = await response.json();
+        const datajson = await response.json();
 
-        console.log(data);
+        const resultado = datajson.collection.items;
 
+        resultado.forEach(item => {
+            const imageUrl = item.links[0].href;
+            const title = item.data[0].title;
+            const description = item.data[0].description;
+            const dateCreated = item.data[0].date_created;
+            
 
-    }
-    
+            document.getElementById("contenedor").innerHTML+=`<h2>${title}</h2>`;
+            document.getElementById("contenedor").innerHTML+=`<img src="${imageUrl}">`;
+            document.getElementById("contenedor").innerHTML+=`<p>${description}</p>`;
+            document.getElementById("contenedor").innerHTML+=`<p>${dateCreated}</p>`;
+            
 
-    
+    })
 
-
-
+ }
 
 });
